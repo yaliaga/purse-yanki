58473df7356c0f135df8adae01896b020c602dbf
Yolinda Aliaga Caisahuana <yaliagca@emeal.nttdata.com>
2022-03-03 18:15:14 -0500

    purse

--- src/main/java/pe/com/purseyanki/model/Purse.java
+++ src/main/java/pe/com/purseyanki/model/Purse.java
@@ -14,7 +14,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @RedisHash("purse")
 public class Purse implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	private String id;
 	private String cardId;
--- src/main/java/pe/com/purseyanki/model/dto/PurseDto.java
+++ src/main/java/pe/com/purseyanki/model/dto/PurseDto.java
@@ -2,6 +2,8 @@ package pe.com.purseyanki.model.dto;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PurseDto implements Serializable{
+	
+	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String cardId;
 }
